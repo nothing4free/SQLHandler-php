6ebc550d4348231b3ffb7083d308d53a3e57030e
Jorge Martínez Hurtado <45176388+nothing4free@users.noreply.github.com>
2021-05-19 22:30:25 +0200

    file: added artwork-groups.php

---
+++ php/artwork-groups.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+  <head>
+	  <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+    <h2 class="header"> Showing all artwork groups </h2>
+
+    <?php
+      // fill this in order for the whole thing to work!
+      $url = "";
+      $user = "";
+      $pass = "";
+      $dbname = "";
+
+      // opens a connection to the database, drops an error if it fails
+      $conn = new mysqli($url, $user, $pass, $dbname);
+      if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+
+      // creates the query on "sql" and executes it, storing the res on "res" 
+      $sql = "SELECT * FROM GrupoDeObras";
+      $res = $conn->query($sql);
+
+      // if the res has more than 0 rows (not empty), it creates an html table and shows the query res data
+      if ($res->num_rows > 0) {
+        echo "<table border='1'><tr><th>Name</th></tr>";
+        while($row = $res->fetch_assoc()) {
+          echo "<tr><td>".$row["Nombre"]."</td></tr>";
+        }
+        echo "</table>";
+      } else {
+        echo "0 ress";
+      }
+      $conn->close();
+    ?>
+
+    <form action="index.html" class="wrapper">
+	    <input type="submit" value="Go back"/>
+    </form>
+
+  </body>
+</html>
